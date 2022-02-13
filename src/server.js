0b33d77c87ce293964ab931b9c425012d00ffb91
FerrariRoma <85842907+wayneglik@users.noreply.github.com>
2022-02-13 20:14:44 +0900

    Setting initial options

---
+++ src/server.js
@@ -0,0 +1,24 @@
+import http from "http";
+import WebSocket from "ws";
+import express from "express";
+
+const app = express();
+
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
+
+app.use("/public", express.static(__dirname + "/public"));
+app.get("/", (req, res) => res.render("home"));
+app.get("/*", (req, res) => res.redirect("/"));
+
+const handleListen = () => console.log(`Listening on http://localhost:4000`);
+
+// app.listen(3000, handleListen);
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+function handleConnection() {}
+
+wss.on("connection", handleConnection);
+
+server.listen(4000, handleListen);
