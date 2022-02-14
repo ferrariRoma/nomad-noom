ceb1d70ce1705813c0faba0988245d3812701ce4
FerrariRoma <85842907+wayneglik@users.noreply.github.com>
2022-02-15 02:46:37 +0900

    Made nickname and content part using stringify and parse with socket object

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import http from "http";
 import WebSocket from "ws";
 import express from "express";
+import { parse } from "path";
 
 const app = express();
 
@@ -13,12 +14,31 @@ app.get("/*", (req, res) => res.redirect("/"));
 
 const handleListen = () => console.log(`Listening on http://localhost:4000`);
 
-// app.listen(3000, handleListen);
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
-function handleConnection() {}
+let sockets = [];
 
-wss.on("connection", handleConnection);
+wss.on("connection", (backSocket) => {
+  sockets.push(backSocket);
+  backSocket["nickname"] = "Anon";
+  console.log("Connected to Browser ✅");
+  backSocket.on("close", () => {
+    console.log("Disconnected from the Browser ❎");
+  });
+  backSocket.on("message", (message) => {
+    const parsed = JSON.parse(message);
+    switch (parsed.type) {
+      case "new_message":
+        sockets.forEach((socket) =>
+          socket.send(`${backSocket.nickname}: ${parsed.payload}`)
+        );
+        break;
+      case "new_nickname":
+        backSocket["nickname"] = parsed.payload;
+        break;
+    }
+  });
+});
 
 server.listen(4000, handleListen);
